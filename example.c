33572d8bdc02ccc815f6eca6ba91354edf269ec4
Jean Ruggiero <45836061+jeanruggiero@users.noreply.github.com>
2020-01-16 18:14:56 -0500

    Updated test function prototypes to return void.

--- example.c
+++ example.c
@@ -38,19 +38,19 @@ void test_is_weekend_with_monday() {
     assert(is_weekend(0) == 0);
 }
 
-int test_is_weekend_with_friday() {
+void test_is_weekend_with_friday() {
     assert(is_weekend(4) == 0);
 }
 
-int test_is_weekend_with_weekend() {
+void test_is_weekend_with_weekend() {
     assert(is_weekend(6) == 1);
 }
 
-int test_is_weekend_with_invalid_input() {
+void test_is_weekend_with_invalid_input() {
     assert(is_weekend(7) == -1);
 }
 
-int test_is_weekend_with_negative_number() {
+void test_is_weekend_with_negative_number() {
     assert(is_weekend(-1) == -1);
 }
 
